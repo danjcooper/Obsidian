1d3b6010508f0f7f2a0beb12efe9fb39f420d1a3
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-04-22 19:13:25 +0100

    update modal style

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -5,7 +5,7 @@ import { props } from './interfaces';
 const Modal = ({ show, close, children }: props) => {
     return show ? (
         <div>
-            <div className={show ? styles.active : styles.inactive}>{children}</div>
+            <div className={styles.active}>{children}</div>
         </div>
     ) : null;
 };
