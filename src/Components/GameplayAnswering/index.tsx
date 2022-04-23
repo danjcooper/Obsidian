1ebc7d406d78d87dab3a29181624ea061e0ae6b8
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-04-23 21:13:56 +0100

    running out of time now counts as loss

--- src/Components/GameplayAnswering/index.tsx
+++ src/Components/GameplayAnswering/index.tsx
@@ -9,27 +9,33 @@ import styles from './style.module.css';
 const GameplayAnswering = ({ housemateData, updateGameState, updateIsWinner, updateRoundPoints }: props) => {
     const [roundData, setRoundData] = useState<roundData | null>(null);
     const [points, setPoints] = useState<number>(100);
-    const [answered, setAnswered] = useState<boolean>(false);
 
     useEffect(() => {
         const interval = setInterval(() => setPoints(points => (points > 0 ? points - 1 : points)), 100);
         return () => clearInterval(interval);
-    }, [answered]);
+    }, []);
 
     useEffect(() => {
         setRoundData(generateRoundData(housemateData));
     }, [housemateData]);
+
+    useEffect(() => {
+        points <= 0 ? endRound(false, 0) : null;
+    }, [points]);
 
     const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
         const id: number = parseInt(e.currentTarget.id);
 
         if (roundData) {
             const result: boolean = id === roundData.winner ? true : false;
-            setAnswered(true);
-            updateIsWinner(result);
-            updateRoundPoints(points);
-            updateGameState(GameStates.RESULT);
+            endRound(result, points);
         }
+    };
+
+    const endRound = (isWinner: boolean, points: number) => {
+        updateIsWinner(isWinner);
+        updateRoundPoints(points);
+        updateGameState(GameStates.RESULT);
     };
 
     return (
@@ -37,7 +43,6 @@ const GameplayAnswering = ({ housemateData, updateGameState, updateIsWinner, upd
             {roundData ? (
                 <>
                     <CountdownTimer points={points} />
-
                     <HousemateCard housemateData={roundData.housemateOne} handleClick={handleClick} />
                     <Question text={roundData.question} />
                     <HousemateCard housemateData={roundData.housemateTwo} handleClick={handleClick} />
