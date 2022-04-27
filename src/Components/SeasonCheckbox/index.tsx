062b7aecdd5668fc6d8bff7c8a7454dd4029338b
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-04-27 13:20:35 +0100

    remove CD from season selection

--- src/Components/SeasonCheckbox/index.tsx
+++ src/Components/SeasonCheckbox/index.tsx
@@ -5,10 +5,12 @@ import styles from './style.module.css';
 const SeasonCheckbox = ({ data, handleClick }: props) => {
     return (
         <>
-            <label className={styles.seasonLabel} htmlFor={data.id.toString()}>
-                {data.name}
-                <input onClick={handleClick} id={data.id.toString()} value={data.seasonCode} type='checkbox' />
-            </label>
+            {data.seasonCode !== 'CD' ? (
+                <label className={styles.seasonLabel} htmlFor={data.id.toString()}>
+                    {data.name}
+                    <input onClick={handleClick} id={data.id.toString()} value={data.seasonCode} type='checkbox' />
+                </label>
+            ) : null}
         </>
     );
 };
