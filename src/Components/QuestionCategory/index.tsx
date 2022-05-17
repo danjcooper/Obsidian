2695c16bc649a20cd6cbc60cde8fb44f752498b8
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-05-17 18:07:22 +0100

    add key to mapped compoen

--- src/Components/QuestionCategory/index.tsx
+++ src/Components/QuestionCategory/index.tsx
@@ -7,7 +7,9 @@ const QuestionCategory = ({ categoryData, index }: props) => {
     return (
         <section id={index.toString()} className={styles.categoryContainer}>
             <h2 className={styles.categoryHeader}>{categoryData.categoryName}</h2>
-            {categoryData.questions.map(question => !question.hidden && <QuestionArticle question={question} />)}
+            {categoryData.questions.map(
+                (question, index) => !question.hidden && <QuestionArticle question={question} key={index} />
+            )}
         </section>
     );
 };
